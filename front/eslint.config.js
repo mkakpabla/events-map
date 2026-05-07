d3e505e2112e26cede903736851f6be2c239d1b2
Michel Akpabla <mk.akpabla@gmail.com>
2026-05-07 15:38:26 +0000

    Update front

---
+++ front/eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.{js,jsx}"],
+    plugins: { react },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        fetch: "readonly",
+      },
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
