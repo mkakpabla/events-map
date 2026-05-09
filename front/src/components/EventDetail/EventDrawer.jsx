f713246b3541232b5a831110f9125a2a42843076
Michel Akpabla <mk.akpabla@gmail.com>
2026-05-09 16:15:19 +0000

    Update front

--- front/src/components/EventDetail/EventDrawer.jsx
+++ front/src/components/EventDetail/EventDrawer.jsx
@@ -190,7 +190,7 @@ export default function EventDrawer({ ev, onClose, theme }) {
                 borderBottom: `1.5px solid ${th.accent}50`,
               }}
             >
-              Voir l'événement
+              Voir l'événement update
               <Icon name="chevronRight" size={12} color={th.accent} />
             </a>
           )}
